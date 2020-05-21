7a868da730495a8d3f7c22b7c7db46e846880956
Gonza Franchino <gonzalo.franchino@gmail.com>
2020-05-21 00:06:05 -0300

    singleton pattern

---
+++ src/main/java/io/gonzajf/designPatterns/creationals/Singleton.java
@@ -0,0 +1,17 @@
+package io.gonzajf.designPatterns.creationals;
+
+public class Singleton {
+
+	private static Singleton instance;
+	
+	private Singleton() {
+	}
+	
+	public static synchronized Singleton getInstance() {
+	
+		if(instance == null ) {
+			instance = new Singleton();
+		}
+		return instance;
+	}
+}
---
+++ src/test/java/io/gonzajf/designPatterns/SingletonTest.java
@@ -0,0 +1,24 @@
+package io.gonzajf.designPatterns;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import io.gonzajf.designPatterns.creationals.Singleton;
+
+public class SingletonTest {
+
+	private static Singleton instance1;
+	private static Singleton instance2;
+	
+	@BeforeAll
+	public static void setUp() {
+		instance1 = Singleton.getInstance();
+		instance2 = Singleton.getInstance();
+	}
+	
+	@Test
+	public void instances_are_same_object() {
+		Assertions.assertTrue(instance1 == instance2);
+	}
+}
