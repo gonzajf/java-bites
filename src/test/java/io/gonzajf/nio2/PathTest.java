dcb032cc48e99d98fd59c46ae5aa1186d826fb5a
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-06-04 11:38:18 -0300

    working with paths

--- src/test/java/io/gonzajf/nio2/PathTest.java
+++ src/test/java/io/gonzajf/nio2/PathTest.java
@@ -1,19 +1,60 @@
 package io.gonzajf.nio2;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class PathTest {
 
-	@Test
-	public void path_creation_test() {
-		Path path1 = Paths.get("src/test/resources/exampleFile.txt");
-		Path path2 = Paths.get("src", "test", "resources", "exampleFile.txt");
-		assertEquals(path1, path2);
+	private static Path path;
+	
+	@BeforeAll
+	static void setUp() {
+		path = Paths.get("src/test/resources/exampleFile.txt");
 	}
 	
+	@Test
+	public void path_creation_test() {
+		Path path2 = Paths.get("src", "test", "resources", "exampleFile.txt");
+		assertEquals(path, path2);
+	}
+	
+	@Test
+	public void path_name_count_test() {
+		assertEquals(4, path.getNameCount());
+	}
+	
+	@Test
+	public void get_file_name_path_test() {
+		assertTrue(path.getFileName() instanceof Path);
+		assertEquals("exampleFile.txt", path.getFileName().toString());
+	}
+	
+	@Test
+	public void get_parent_path_test() {
+		assertTrue(path.getParent() instanceof Path);
+		assertEquals("src/test/resources", path.getParent().toString());
+	}
+	
+	//is null because it's a relative path
+	@Test
+	public void get_root_path() {
+		assertEquals(null, path.getRoot());
+	}
+	
+	@Test
+	public void test_relative() {
+		assertFalse(path.isAbsolute());
+	}
+	
+	@Test
+	public void convert_to_absoluth_path_test() {
+		assertTrue(path.toAbsolutePath().isAbsolute());
+	}
 }
