fe2c0006696f98ce963f95faf6dec37a65ef3205
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-09-03 17:03:37 -0300

    runnable interface vs extending thread class

--- src/main/java/io/gonzajf/concurrency/PrintData.java
+++ src/main/java/io/gonzajf/concurrency/PrintData.java
@@ -10,10 +10,13 @@ public class PrintData implements Runnable {
 	}
 
 	public static void main(String[] args) {
+		//the execution order is unknown until 
+		//this sample uses four the total of four threads:
+		//the main user thread and the three additional threads created by the main method.
 		System.out.println("begin");
-		(new ReadInventoryThread()).run();;
-		(new Thread(new PrintData())).run();
-		(new ReadInventoryThread()).run();
+		(new ReadInventoryThread()).start();
+		(new Thread(new PrintData())).start();
+		(new ReadInventoryThread()).start();
 		System.out.println("end");
 	}
 }
